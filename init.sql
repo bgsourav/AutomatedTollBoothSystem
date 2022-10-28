1cf233591c068a74eb4473ce9e3fe05ac40b4df1
AdityaSRaj <85154011+AdityaSRaj@users.noreply.github.com>
2022-10-28 18:33:59 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -6,6 +6,6 @@ create database TollBoothManagementSystem;
  use TollBoothManagementSystem;
  --Creation of fare table
  create table Fare_Details(Type varchar(40),Fare integer,PRIMARY KEY(Type));
- --Insertion of values into the table
+ --Insertion of values into the fare table
  insert into Fare_Details values('Class 1',35),('Class 2',45),('Class 3',60),('Class 4',135),('Class 5','195'),('Class 6',195),('Class 7',195),('Class 8',195),('Class 9',210),('Class 10',235),('Class 11',250),('Class 12',325);
  
