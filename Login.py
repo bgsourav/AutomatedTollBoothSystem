e19ef76f0e726780b248662f3d9189afb6e12acf
Hrishi34 <83940584+Hrishi34@users.noreply.github.com>
2022-11-17 22:07:50 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -7,6 +7,15 @@
 # CREATE USER 'Staff1'@'%' IDENTIFIED BY '1234';
 
 
+#commands to be typed in mysql/bin before running this program
+# initially login as root and then type these commands
+
+# CREATE USER 'Admin'@'%' IDENTIFIED BY '1234';
+# GRANT ALL PRIVILEGES ON *.* TO Admin;
+# GRANT GRANT OPTION ON *.* TO Admin;
+# CREATE USER 'Staff1'@'%' IDENTIFIED BY '1234';
+
+
 import mysql.connector
 
 
@@ -32,23 +41,86 @@ def login():
           str1='select current_user()'
           cursor.execute(str1)
           res=cursor.fetchone()
-          if("Admin@%" in res):
-               print("Hello Admin!")
-          elif any("Staff" in s for s in res):
-               print("Hello Staff")
+          print(res)
+          if("ADMIN01@%" in res or "ADMIN02@%" in res):
+               print("Logged in as Admin..")
           else:
-               print("Hello customer")
+               print("Logged in as Staff..")
           return db
 
 
-print("\nWelcome to ATM\n")
-print("<Fare details here..>")
-ch=input("Do you want to login? [Y/N]\n")
-while(ch in ['y','Y']):
-     db= login()
-     if(db):
-          break;
-     else:
-          ch=input("Would you like to try again? [Y/N]\n")
-if (ch in ['N','n']) :
-     print("Ok. Thank You!")
+def car_entered(db,mycursor):
+     reg_no=input("Please Enter the car Registration number : ")
+     str1=f"select * from vehicle_Details where Registration_number= '{reg_no}'"
+     mycursor.execute(str1)
+     res1=mycursor.fetchone()
+     if(res1):      #if car is present in database
+          car_present(db,mycursor,reg_no)
+     # else:        #if car is not present in database
+     #      car_notpresent(db,mycursor,reg_no)
+
+def car_present(db,mycursor,reg_no):
+     str1=f"Select check_flag from vehicle_details where check_flag=2 and Registration_Number = '{reg_no}'"
+     mycursor.execute(str1)
+     str1=mycursor.fetchone()
+     if (str1):
+          print("This is a government vehicle and is exempted from tax..")
+          return
+     toll_no=int(input(("Please enter the toll booth number where the vehicle entered")))
+     str2=f"Select Toll_Booth_No from Toll_Booth where Toll_Booth_No={toll_no}"
+     mycursor.execute(str2)
+     str2=mycursor.fetchone()
+     if(str2==0):   #balance is less than fare
+          # ask passenger to add money to account
+          print("Your account balance is low..")
+          balanc=f"select distinct balance from Transaction_Details natural join Vehicle_Details natural join Account_Details where Vehicle_Details.Registration_Number='{reg_no}'"
+          mycursor.execute(balanc)      #get balance 
+          balanc=mycursor.fetchone()
+          print("Balance : ",balanc)
+          faree=f"select Toll_Price from Fare_Table natural join Vehicle_Details where Vehicle_Details.Registration_Number={reg_no}"
+          mycursor.execute(faree)       #get toll price
+          faree=mycursor.fetchone()
+          print("Please recharge now.")
+          recharg=int(input(f"Enter the recharge amount (Minimum amount : {faree}). "))
+          while(recharg<faree):
+               print("Please enter minimum amount")
+               recharg=int(input(f"Enter the recharge amount (Minimum amount : {faree}). "))
+          balanc+=recharg-faree
+          print("Recharge successful..")
+          str3=f"UPDATE Account_Details natural JOIN Transaction_Details SET Account_Details.balance = {balanc} WHERE Transaction_Details.Registration_Number = '{reg_no}'"
+          mycursor.execute(str3)       #update the balance 
+          print("Tax deducted sucessfully..\n Balance amount has been updated")
+          print("Balance amount : ",balanc)
+     #      if (balanc>faree):          #check if newbalance is low
+               
+     # else:
+     #      pass
+
+     
+
+# def car_enter(db):
+#      #enter car details
+     #  ask for tolbooth numbr
+#      check for it in car details table
+#      if present:
+#           check bit:
+#                deduct balance
+#           else if:
+#                set check bit to 0
+#                give Warning
+#                ask for payemnt
+#           else:
+#                leave without tax for govt vehicles
+#           update transac details
+#           update toll booth revenue
+#           if balance_new > fare:
+#                update check bit
+#      else:
+#           insert into car details table
+#           ask payment
+#           insert into transac
+#           if balance_new > fare:
+#                update check bit
+
+
+              
