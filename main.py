e18f56841ccc171bafd37af159510aca5494a3f0
Hrishi34 <83940584+Hrishi34@users.noreply.github.com>
2022-11-18 18:01:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ print("<Fare details here..>")
 ch=input("Do you want to login? [Y/N]\n")
 while(ch in ['y','Y']):
      db= login()
-     if(db):
+     while(db ):
           cursor=db.cursor()
           car_entered(db,cursor)
-     else:
+
+          ch1=input("Would you like to logout?[Y/N] ")
+          if(ch1 in ['y','Y']):
+               ch='n'
+               break
+     if (ch in ['Y','y']):
           ch=input("Would you like to try again? [Y/N]\n")
 if (ch in ['N','n']) :
      print("Ok. Thank You!")
+else:
+     print("Please enter a valid choice")
