7df8d2061e25bbd9cde910234fdf5e27410563cb
bgsourav <73071764+bgsourav@users.noreply.github.com>
2022-11-26 13:15:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,21 +15,22 @@ cus_tab.add_row(["Two_axle_truck",75])
 cus_tab.sortby = 'Toll_Price'
 print(cus_tab)
 print("")
-ch=input("Do you want to login? [Y/N]\n")
-while(ch in ['y','Y']):
-     db= login()
-     while(db ):
-          cursor =db.cursor(buffered=True)
-          car_entered(db,cursor)
-          print("Adding staff")
-          create_staff(db,cursor,global_usercount)
-          ch1=input("Would you like to logout?[Y/N] ")
-          if(ch1 in ['y','Y']):
-               ch='n'
-               break
-     if (ch in ['Y','y']):
-          ch=input("Would you like to try again? [Y/N]\n")
-if (ch in ['N','n']) :
-     print("Ok. Thank You!")
-else:
-     print("Please enter a valid choice")
+while True:
+     ch=input("Do you want to login? [Y/N]\n")
+     while(ch in ['y','Y']):
+          db= login()
+          while(db ):
+               cursor =db.cursor(buffered=True)
+               car_entered(db,cursor)
+               print("Adding staff")
+               create_staff(db,cursor,global_usercount)
+               ch1=input("Would you like to logout?[Y/N] ")
+               if(ch1 in ['y','Y']):
+                    ch='n'
+                    break
+          if (ch in ['Y','y']):
+               ch=input("Would you like to try again? [Y/N]\n")
+     if (ch in ['N','n']) :
+          print("Ok. Thank You!")
+     else:
+          print("Please enter a valid choice")
