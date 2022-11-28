ea920950d8fad9832087b97d66fdbea49c8d2ee4
Hrishi34 <83940584+Hrishi34@users.noreply.github.com>
2022-11-28 23:45:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from prettytable import PrettyTable
 import inquirer
 from simple_chalk import chalk,green,red,yellowBright,redBright,greenBright,blueBright,cyan
 global global_usercount
-global_usercount=3
+global_usercount=4
 def run():
-     print(yellowBright.bold("\nWelcome to ATM\n"))
+     print(yellowBright.bold("\nWelcome to Automated Toll Booth Machine\n"))
      print(cyan("Fare details here.."))
      print("")
-# Custom_table = cus_tab
+     # Custom_table = cus_tab
      cus_tab = PrettyTable()
      cus_tab.field_names = ["Vehicle_Type","Toll_Price"]
      cus_tab.add_row(["Four_axle_truck",150])
@@ -17,7 +17,6 @@ def run():
      cus_tab.add_row(["Two_axle_truck",75])
      cus_tab.sortby = 'Toll_Price'
      print(cus_tab)
-     print("")
      while True:
           ch=input(blueBright("Do you want to login? [Y/N]\n")) #Simple_chalk functions can be nested with '.' and are callable like a function
           while(ch in ['y','Y']):
@@ -27,9 +26,9 @@ def run():
                if(res != None):
                     db = res[0]
                     is_admin = res[1]
-          # print(db)
                while(db):
                     cursor = db.cursor(buffered = True)
+                    print("\n")
                     if is_admin == 1:
                          operations = [
                               inquirer.List('op_x',
@@ -45,17 +44,18 @@ def run():
                               ),
                          ]
                     answer = inquirer.prompt(operations)['op_x']
-               #'op_x' refers to the operation of which the answer is of
-               #the inquirer prompt returns a Dicitonary which is of the form
-               #{'op_x':Choice}
+                    #'op_x' refers to the operation of which the answer is of
+                    #the inquirer prompt returns a Dicitonary which is of the form
+                    #{'op_x':Choice}
                     if(answer in ['1.)Enter Car Details','2.)Enter Car Details']):
                          car_entered(db,cursor)
-                    if("Create New User" in answer):
+                    elif("Create New User" in answer):
                          create_staff(db,cursor,global_usercount)
-                    ch1=input("Are you sure you want to Log Out?[Y/N] ")
-                    if(ch1 in ['y','Y']):
-                         ch='n'
-                         break
+                    else:
+                         ch1=input("Are you sure you want to Log Out?[Y/N] ")
+                         if(ch1 in ['y','Y']):
+                              ch='n'
+                              break
                if (ch in ['Y','y']):
                     ch=input("Would you like to try again? [Y/N]\n")
           if (ch in ['N','n']) :
