1418f540707ec9dcb17ca040c8b7eb9d7c3aa745
AdityaSRaj <85154011+AdityaSRaj@users.noreply.github.com>
2022-11-29 12:04:03 +0530

    Add files via upload

--- validation/recharge.py
+++ validation/recharge.py
@@ -3,12 +3,13 @@ from pprint import pprint
 import inquirer  
 
 def recharge_validation(answers, current):
-    if not re.match("^\d{1,10}$",current):
+
+    if not re.match("^\d{1,10}$", current):
         raise inquirer.errors.ValidationError("", reason="Invalid format!")
     return True
 
 questions = [
-    inquirer.Text("Recharge",message="Enter recharge amount",validate=recharge_validation)
+    inquirer.Text("Recharge",message="Enter recharge",validate=recharge_validation)
 ]
 answers = inquirer.prompt(questions)
 pprint(answers)
