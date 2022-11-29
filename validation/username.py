0925dac9e1ec4600a93da835bcf94dd20777cbe4
AdityaSRaj <85154011+AdityaSRaj@users.noreply.github.com>
2022-11-29 20:42:05 +0530

    Update username.py

--- validation/username.py
+++ validation/username.py
@@ -6,7 +6,7 @@ import inquirer
 print('User Name must be User Type(ADM/USR) followed by three numbers')
 
 def username_validation(answers, current):
-    if not re.match("USR[0-9]{2}[1-9]$|ADM[0-9]{2}[1-9]$", current):
+    if not re.match("STF[0-9]{2}[1-9]$|ADM[0-9]{2}[1-9]$", current):
         raise inquirer.errors.ValidationError("", reason="Invalid user name format!")
     return True
 
