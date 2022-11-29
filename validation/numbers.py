bf467f41f88a034a1f50988146b7306ac2f9ad1e
AdityaSRaj <85154011+AdityaSRaj@users.noreply.github.com>
2022-11-29 15:06:30 +0530

    Update numbers.py

--- validation/numbers.py
+++ validation/numbers.py
@@ -1,1 +1,16 @@
 
+import re
+from pprint import pprint
+import inquirer  
+
+
+def digits_validation(answers, current):
+    if not re.match("^[0-9]{6}$", current):
+        raise inquirer.errors.ValidationError("", reason="Invalid format!")
+    return True
+
+questions = [
+    inquirer.Text("Number",message="Enter number",validate=phone_validation)
+]
+answers = inquirer.prompt(questions)
+pprint(answers)
